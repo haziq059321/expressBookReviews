6fbb535776f64e95f7527dfd04fc5e50ef58fdb8
haziq059321 <haziqshiekh824@gmail.com>
2023-08-29 07:57:52 +0500

    add changes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,21 +7,71 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let userswithsamename = users.filter((user)=>{
+  return user.username === username
+});
+if(userswithsamename.length > 0){
+  return true;
+} else {
+  return false;
+}
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validusers = users.filter((user)=>{
+  return (user.username === username && user.password === password)
+});
+if(validusers.length > 0){
+  return true;
+} else {
+  return false;
+}
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
+  // const review = req.params.review;
+  //   let book = books[reeview]
+  //   if (book) { //Check is friend exists
+  //       let DOB = req.body.DOB;
+  //       //Add similarly for firstName
+  //       //Add similarly for lastName
+  //       //if DOB the DOB has been changed, update the DOB 
+  //       if(DOB) {
+  //           friend["DOB"] = DOB
+  //       }
+  //       //Add similarly for firstName
+  //       //Add similarly for lastName
+  //       friends[email]=friend;
+  //       res.send(`Friend with the email  ${email} updated.`);
+  //   }
+  //   else{
+  //       res.send("Unable to find friend!");
+  //   }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
